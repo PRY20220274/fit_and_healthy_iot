bf6baa0dfce085492ea53f700dbd54bbbead1d5d
MarioTataje <magotor1304@gmail.com>
2023-03-11 08:30:23 -0500

    feat: agregar servicios

--- src/config/fit.js
+++ src/config/fit.js
@@ -1,4 +1,33 @@
 const axios = require("axios");
+const {google} = require('googleapis');
+
+const auth = new google.auth.GoogleAuth({
+    // Llave de autenticación en JSON descargada desde la consola de Google Cloud.
+    keyFile: './path/to/credentials.json', 
+    // Ámbito de la API de Google Fit.
+    scopes: [
+      'https://www.googleapis.com/auth/fitness.activity.read',
+      'https://www.googleapis.com/auth/fitness.body.read',
+    ],
+  }
+);
+
+// Crea un cliente de la API de Google Fit.
+const fitness = google.fitness({
+    version: 'v1',
+    auth: auth,
+  }
+);
+  
+fitness.users.dataSources.datasets.get({
+    userId: 'me',
+    dataSourceId: 'derived:com.google.step_count.delta:com.google.android.gms:estimated_steps',
+    datasetId: `${startDate}-${endDate}`,
+  }, (err, res) => {
+    if (err) throw err;
+    console.log(res.data);
+  }
+);
 
 const url = 'https://www.googleapis.com/fitness/v1/users/me/dataSources';
 
